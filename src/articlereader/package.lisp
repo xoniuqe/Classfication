9c1f171b81e684afc559c2bf0abeaaa5adf9a5e7
Tobias Arens <arenst@hochschule-trier.de>
2016-10-14 13:23:44 +0200

    parallel and sequence keywords used in setup

--- src/articlereader/package.lisp
+++ src/articlereader/package.lisp
@@ -1,8 +1,8 @@
 ;;;; package.lisp
 
 (defpackage #:articlereader
-  (:use #:cl :drakma :cl-ppcre :cl-html-parse)
-  (:export :read-structure :match-struct-argument :match-page-struct :ignore-lists :test :text :headline :date :ignore :place)
+  (:use #:cl :drakma :cl-ppcre :closure-html)
+  (:export :fetch-article :parse-html :is-struct-placeholder :read-structure :match-struct-argument :match-page-struct :ignore-lists :test :text :headline :date :ignore :place :sequence :parallel)
  )
 
   
