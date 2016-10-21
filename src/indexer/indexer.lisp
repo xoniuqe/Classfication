84b86698175e285002b9621b0b7f6d221942ff8a
Tobias Arens <arenst@hochschule-trier.de>
2016-10-21 14:08:22 +0200

    added empty core methods

--- src/indexer/indexer.lisp
+++ src/indexer/indexer.lisp
@@ -2,3 +2,13 @@
 
 (in-package #:indexer)
 
+(defun make-index (document &optional (indexed-document (new-index)))
+
+)
+
+
+;create a new symbol for the indexed document
+(defun new-index ()
+ 	(let ((index (gensym "DOC-INDEX-")))
+  		(import index)
+  		index))
