eb3722d28c41fa195a2ceac803d86d95db8418b3
Tobias Arens <arenst@hochschule-trier.de>
2016-10-21 14:08:12 +0200

    added empty core methods

--- src/classificator/classificator.lisp
+++ src/classificator/classificator.lisp
@@ -1,3 +1,9 @@
 ;;;; classificator.lisp
 
 (in-package #:classificator)
+
+; learns to connect the list of documents to a certain class
+(defun learn-class (indexed-documents class))
+
+; when the algorithm collected enough data we can try to get the classes of a certain document even if it is a new one
+(defun get-classes (indexed-document))
