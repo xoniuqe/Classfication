533121647db87f66185bf9020f3feb13beba461c
Tobias Arens <arenst@hochschule-trier.de>
2016-10-22 16:49:49 +0200

    added some links

--- src/classificator/classificator.lisp
+++ src/classificator/classificator.lisp
@@ -7,3 +7,8 @@
 
 ; when the algorithm collected enough data we can try to get the classes of a certain document even if it is a new one
 (defun get-classes (indexed-document))
+
+;https://en.wikipedia.org/wiki/Naive_Bayes_classifier
+;https://en.wikipedia.org/wiki/Tf%E2%80%93idf
+
+;https://www.reddit.com/r/MachineLearning/comments/1inxnq/how_to_factor_in_tfidf_with_naive_bayes/
