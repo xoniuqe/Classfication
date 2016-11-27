3ea81a53f6748eb7d6ed72e86f6d7530ec178e2a
Tobias Arens <arenst@hochschule-trier.de>
2016-11-27 22:49:24 +0100

    upgraded setup for previous changes

--- src/setup.lisp
+++ src/setup.lisp
@@ -51,17 +51,23 @@
   (load-indexer)
   (load-classificator)
   (load-data)
-  ;(ql:quickload :uiop)
-  ;(load-trivial-browser)
-  ;(load-gui)
+  (ignore-errors (progn (ql:quickload :uiop)
+  (load-trivial-browser)
+  (load-gui)))
 )
 
 (setup)
 
+;benötigtes setup
 (load-trivial-browser)
 (trivial-open-browser:open-browser "https://www.spiegel.de")
 
 (data:read-categories (current-pathname "../data/categories" "txt"))
+
+(data:read-page-structures (current-pathname "../data/pagestructure" "txt"))
+(data:get-pagestructure-types)
+(data:get-pagestructure "spiegel")
+
 
 (data:get-category-name 1)
 
@@ -70,22 +76,54 @@
 
 (data:read-structures (current-pathname "../data/structure" "txt"))
 
-(second (data:get-struct "sueddeutsche"))
+(first (data:get-struct "sueddeutsche"))
 
 
 (data:set-webfetcher (defun webfetcher (link)
   (webengine++lisp-webfetcher 0 link :want-string T)
 ))
 
-(data:build-classificator (current-pathname "../data/categories" "txt") (current-pathname "../data/structure" "txt") (list (current-pathname "../data/new-data" "txt")))
+;eventuell classificator speichern?
+(data:build-classificator (current-pathname "../data/categories" "txt") (current-pathname "../data/structure" "txt") (list (current-pathname "../data/spiegel-data" "txt")))
 
 (mapcar (lambda (tuple) 
           (list (first tuple) (get (get (first tuple) 'INDEXER:DOCUMENT) 'ARTICLEREADER:HEADLINE) (second tuple))
 ) (data:test-classificator))
 
 (load-gui)
+(gui:set-categories (mapcar 'second (data:get-categories)))
+(gui:set-classes (mapcar 'second (get (classificator:get-corpus) 'CLASSIFICATOR:CLASSES)))
 (gui:define-interface)
+
 (gui:display)
+
+(setq *source-pages* '(("spiegel" "http://www.spiegel.de/politik/deutschland" "http://www.spiegel.de") ("sueddeutsche" "http://www.sueddeutsche.de/politik" "")))
+
+
+(gui:set-search-function (lambda (term categories) 
+                  (mapcan (lambda (source) (let* ((struct (data:get-pagestructure source))
+                                                        (pagelink (second (find source *source-pages* :key #'first :test #'string-equal)))
+                                                        (prefix (nth 2 (find source *source-pages* :key #'first :test #'string-equal)))
+                                                        (article (articlereader:fetch-article (webengine++lisp-webfetcher 0 pagelink :want-string T) struct '()))
+                                                        (teasers (remove-if (lambda (elem) (equal elem :TEASER)) (get article 'ARTICLEREADER:TEASERS))))
+(mapcan (lambda (teaser) 
+
+          
+          (let* ((tarticle (articlereader:fetch-article (webengine++lisp-webfetcher 0 (concatenate 'string prefix teaser) :want-string T) (first (data:get-struct source)) (second (data:get-struct source))))
+                (class  (classificator:classify-document  (indexer:make-index tarticle)))
+                (class-name (first (first class)))
+                (class-value (second class)))
+                 (cond ((and categories (member class-name categories) (<= class-value -0.015) )                                          
+                             (list (list (get tarticle 'ARTICLEREADER:HEADLINE) class-name class-value (concatenate 'string prefix teaser))))
+                       ((and (not categories) (<= class-value -0.015))  (list (list (get tarticle 'ARTICLEREADER:HEADLINE) class-name class-value (concatenate 'string prefix teaser))))
+                       ((not categories) (list (list (get tarticle 'ARTICLEREADER:HEADLINE) "" 0 (concatenate 'string prefix teaser))))
+                       (T NIL))
+                                                                       )) teasers)        
+                                             )) (data:get-pagestructure-types))
+                  
+
+))
+
 
 ;Drakma needs openSSl 1.0.1, the version 1.1.0 removed to much functionality
 
