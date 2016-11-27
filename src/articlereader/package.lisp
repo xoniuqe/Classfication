fdf7eb27604695cb0b350c5014dcfd58e08b4c07
Tobias Arens <arenst@hochschule-trier.de>
2016-11-27 22:47:15 +0100

    added export to teaser

--- src/articlereader/package.lisp
+++ src/articlereader/package.lisp
@@ -2,7 +2,7 @@
 
 (defpackage #:articlereader
   (:use #:cl :drakma :cl-ppcre :closure-html)
-  (:export :fetch-article :parse-html :is-struct-placeholder :read-structure :match-struct-argument :match-page-struct :ignore-lists :test :text :headline :date :ignore :place :sequence :parallel)
+  (:export :fetch-article :parse-html :is-struct-placeholder :read-structure :match-struct-argument :match-page-struct :ignore-lists :test :text :headline :date :ignore :place :sequence :parallel :TEASER :TEASERS)
  )
 
   
