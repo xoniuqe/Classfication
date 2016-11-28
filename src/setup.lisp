0c98be87ccab6ddb2b2a29d3d4bbb86fdba429a2
Tobias Arens <arenst@hochschule-trier.de>
2016-11-28 10:25:25 +0100

    removed unnecessary code parts

--- src/setup.lisp
+++ src/setup.lisp
@@ -47,6 +47,7 @@
 
   (load-util)
   (load-articlereader)
+  ;Drakma needs openSSl 1.0.1, the version 1.1.0 removed to much functionality
   (load (current-pathname "website-crawler/new_iis_start_small"))
   (load-indexer)
   (load-classificator)
@@ -125,106 +126,4 @@
 
 (gui:display)
 
-;Drakma needs openSSl 1.0.1, the version 1.1.0 removed to much functionality
 
-
-(setf *html-page* (webengine++lisp-webfetcher 0 "http://www.sueddeutsche.de/politik/bundesregierung-falsche-richtung-spd-1.3154952" :want-string T))
-
-(setf *html-page2* (webengine++lisp-webfetcher 0 "http://www.sueddeutsche.de/politik/rechtsextremismus-neonazis-besitzen-schusswaffen-1.2895449" :want-string T))
-
-;(:DIV ((:CLASS "header")) ("8. September 2016, 12:54 Uhr" (:H2 NIL ((:STRONG NIL ("Türkei")) "Türkei erlaubt Bundestagsabgeordneten Reise nach Incirlik"))))
-
-(setf *sued-structure*
-      '(:SEQUENCE (:DIV ((:CLASS "header")) (:SEQUENCE (:DIV ((:DATETIME :DATE) (:CLASS "timeformat"))) (:H2 NIL (:SEQUENCE :IGNORE :HEADLINE))))
-        (:DIV ((:CLASS "body") (:ID "article-body")) (:PARALLEL (:P NIL :TEXT)))))
-
-(setf *sued-link-structure* '((:A ((:DATA-PAGETYPE "THEME") (:CLASS "themelink")) (:SEQUENCE :TEXT))))
-
-
-
-(setf article (articlereader:fetch-article *html-page* *sued-structure* *sued-link-structure*))
-
-
-(setf article2 (articlereader:fetch-article *html-page2* *sued-structure* *sued-link-structure*))
-
-
-(setf index (indexer:make-index article))
-
-(setf index2 (indexer:make-index article2))
-
-
-
-(load-classificator)
-
-(classificator:setup)
-(setq testclass (classificator:new-document-class "test"))
-(setq testclass2 (classificator:new-document-class "test2"))
-
-;classificator tests
-(classificator:add-document testclass index)
-
-(classificator:add-document testclass2 index2)
-
-
-
-
-(classificator:calculate-corpus-metrics)
-
-(classificator:classify-document index2)
-
-
-
-
-(setf *html-page* (webengine++lisp-webfetcher 0 "http://www.spiegel.de/wirtschaft/soziales/fluechtlinge-in-deutschland-sind-oft-ueberqualifiziert-a-1111237.html" :want-string T))
-
-(setf *spon-politik-deutsch* (webengine++lisp-webfetcher 0 "http://www.spiegel.de/politik/deutschland/" :want-string T))
-
-(setf *spon-structure*
-      '(:SEQUENCE (:SPAN ((:CLASS "headline-intro")) :HEADLINE-INTRO)
-        (:SPAN ((:CLASS "headline")) :HEADLINE)
-        (:P ((:CLASS "article-intro")) (:SEQUENCE (:STRONG NIL :TEXT)));:ARTICLE-INTRO)))
-        (:DIV ((:CLASS "timeformat") (:ITEMPROP "datePublished") (:DATETIME :DATE)) ()); (:SEQUENCE (:SPAN ((:CLASS "article-function-date")) (:SEQUENCE :IGNORE :DATE :IGNORE)))) 
-        
-        (:DIV ((:CLASS "article-section clearfix") (:ITEMPROP "articleBody")) (:PARALLEL (:P NIL :TEXT))))
-)
-
-;#content-main
-;(setf *spon-category-structure*
- ;     '(:SEQUENCE (:DIV ((:ID "content-main") (:CLASS "grid-channel spSmallScreen clearfix"))  
-  ;                      (:PARALLEL (:DIV ((:CLASS "column-both")) (:PARALLEL (:DIV ((:CLASS "teaser teaser-first")) (:PARALLEL (:H2 ((:CLASS "article-title ")) :TEASER)))))))
-   ;               (:DIV ((:CLASS "column-both main-content")) 
-    ;                    (:PARALLEL (:DIV ((:CLASS "teaser teaser-first")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) :TEASER)))))
-     ;                              (:DIV ((:CLASS "teaser")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) :TEASER)))))))))
-
-
-(setf *spon-category-structure*
-      '(:SEQUENCE (:DIV ((:ID "content-main") (:CLASS "grid-channel spSmallScreen clearfix"))  
-                        (:PARALLEL (:DIV ((:CLASS "column-both")) (:PARALLEL (:DIV ((:CLASS "teaser teaser-first")) (:PARALLEL (:H2 ((:CLASS "article-title ")) (:PARALLEL (:A ((:ID :TEASER) (:TITLE :IGNORE)) NIL)))))))))
-                  (:DIV ((:CLASS "column-both main-content")) 
-                        (:PARALLEL (:DIV ((:CLASS "teaser teaser-first")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) (:PARALLEL (:A ((:ID :TEASER) (:TITLE :IGNORE)) NIL)))))))
-                                   (:DIV ((:CLASS "teaser")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) (:PARALLEL (:A ((:ID :TEASER) (:TITLE :IGNORE)) NIL)))))))))))
-
-
-(articlereader:parse-html *html-page*)
-
-(articlereader:fetch-article *spon-politik-deutsch* *spon-category-structure* '())
-
-
-(load-articlereader)
-(articlereader:fetch-article *html-page* *spon-structure* '())
-
-
-(setf *sueddeutsche-politik* (webengine++lisp-webfetcher 0 "http://www.sueddeutsche.de/politik" :want-string T))
-
-;(setf *sueddeutsche-category-structure*
- ;     '(:SEQUENCE (:DIV ((:ID "wrapper"))
-  ;                      (:PARALLEL (:DIV ((:ID "sitecontent")(:CLASS "mainpage")(:ROLE "main")) (:PARALLEL (:DIV ((:CLASS "teaser toptop")) :TEASER) (:DIV ((:CLASS "teaser top")) :TEASER)))))))
-
-(setf *sueddeutsche-category-structure*
-      '(:SEQUENCE (:DIV ((:ID "wrapper"))
-                        (:PARALLEL (:DIV ((:ID "sitecontent")(:CLASS "mainpage")(:ROLE "main")) (:PARALLEL (:DIV ((:CLASS "teaser toptop")) (:PARALLEL (:A ((:ID :TEASER) (:CLASS "entry-title") (:REL "bookmark") (:DATA-PAGETYPE "STANDARD_ARTICLE") (:DATA-ID :IGNORE)) ()))) (:DIV ((:CLASS "teaser top")) (:PARALLEL (:A ((:ID :TEASER) (:CLASS "entry-title") (:REL "bookmark") (:DATA-PAGETYPE "STANDARD_ARTICLE") (:DATA-ID :IGNORE)) ())))))))))
-                 ; (:DIV ((:CLASS "column-both main-content")) 
-                  ;      (:PARALLEL (:DIV ((:CLASS "teaser teaser-first")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) :TEASER)))))
-                   ;                (:DIV ((:CLASS "teaser")) (:SEQUENCE (:DIV ((:CLASS "clearfix")) (:PARALLEL (:H2 ((:CLASS "article-title ")) :TEASER)))))))))
-
-(articlereader:fetch-article *sueddeutsche-politik* *sueddeutsche-category-structure* '())
